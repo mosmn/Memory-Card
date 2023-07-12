2b55e8301102a428937a48281c49e959f3703379
mosmn <ithinkisee8@gmail.com>
2023-07-12 14:29:00 +0800

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './styles/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, React 18!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
